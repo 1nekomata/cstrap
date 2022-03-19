8599ea212fe4363cfd7ac89a5fc5fe71495ae654
1nekomata <sp.pflaumer@gmail.com>
2022-03-19 18:44:37 +0000

    move most variables to the top and move the if statement checking for argv[2] higher up

--- main.c
+++ main.c
@@ -25,53 +25,49 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    char *buf = NULL;
-    int cnt = 0, len = 0;
-
-    char *headers = NULL;
-
     if (argv[1] == NULL){
         printf("No filename specified!\n");
         exit(1);
     }
 
-    for(int i = 2; i <= argc; i++){
-        cnt++;
-    }
-
-    for(int i = 2; i <= cnt; i++){
-        //printf("cnt = %d, i = %d\n", cnt, i);
-        //printf("argv[%d] = %s\n", i, argv[i]);
-        len += strlen(argv[i]) + strlen("#include <>\n") + 1;
-        //printf("len = %d\n", len);
-    }
-
-    headers = realloc(headers, len + 15);
-
     char *temp = NULL;
-
-    for(int i = 2; i < argc; i++){
-        temp = realloc(temp, strlen("#include <>\n") + strlen(argv[i]) + 1);
-        sprintf(temp, "#include <%s>\n", argv[i]);
-        strcat(headers, temp);
-    }
-
+    char *buf = NULL;
+    int cnt = 0, len = 0, fd;
+    char *headers = NULL;
     char *mainfuncpart1 = "#include <stdio.h>\n";
     char *mainfuncpart2 = "\nint main(int argc, char *argv[]){\n\n\n    return 0;\n}\n";
-
     char *mainfunc = NULL;
 
     if(argv[2] != NULL){
+        for(int i = 2; i <= argc; i++){
+        cnt++;
+        }
+
+        for(int i = 2; i <= cnt; i++){
+            //printf("cnt = %d, i = %d\n", cnt, i);
+            //printf("argv[%d] = %s\n", i, argv[i]);
+            len += strlen(argv[i]) + strlen("#include <>\n") + 1;
+            //printf("len = %d\n", len);
+        }
+
+        headers = realloc(headers, len + 15);
+
+        for(int i = 2; i < argc; i++){
+            temp = realloc(temp, strlen("#include <>\n") + strlen(argv[i]) + 1);
+            sprintf(temp, "#include <%s>\n", argv[i]);
+            strcat(headers, temp);
+        }
+
         mainfunc = realloc(mainfunc, len + strlen(temp) + strlen(mainfuncpart1) + strlen(mainfuncpart2));
 
         strcat(mainfunc, mainfuncpart1);
         strcat(mainfunc, headers);
         strcat(mainfunc, mainfuncpart2);
+
     } else {
         mainfunc = realloc(mainfunc, strlen("#include <stdio.h>\n\nint main(int argc, char *argv[]){\n\n\n    return 0;\n}\n"));
         mainfunc = "#include <stdio.h>\n\nint main(int argc, char *argv[]){\n\n\n    return 0;\n}\n";
     }
-    int fd;
 
     creat(argv[1], 0700);
 
